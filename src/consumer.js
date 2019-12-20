8757ee41a6b0c15b1aeafe8f02a3e0c3a619d775
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2019-12-20 16:02:19 +0530

    introducing retry count

--- src/consumer.js
+++ src/consumer.js
@@ -3,35 +3,50 @@ const Promise = require('bluebird');
 const config = require('config');
 const logger = require('./common/logger');
 const consumer = new Kafka.GroupConsumer();
-const { producerLog, pAuditLog } = require('./api/audit')
-const { consumerLog, cAuditLog } = require('./api/audit')
+const {
+  producerLog,
+  pAuditLog
+} = require('./api/audit')
+const {
+  consumerLog,
+  cAuditLog
+} = require('./api/audit')
 //const { migrateDelete, migrateInsert, migrateUpdate } = require('./api/migrate')
-const { migratepgDelete, migratepgInsert, migratepgUpdate } = require('./api/migratepg')
-const { migrateifxinsertdata,migrateifxupdatedata } = require('./api/migrateifxpg')
+const {
+  migratepgDelete,
+  migratepgInsert,
+  migratepgUpdate
+} = require('./api/migratepg')
+const {
+  migrateifxinsertdata,
+  migrateifxupdatedata
+} = require('./api/migrateifxpg')
+const pushToKafka = require('./api/pushToKafka')
 //const { migrateinsertdata } =  require('./api/migrate-data')
 const producer = new Kafka.Producer()
 
 producer.init().then(function () {
   console.log('connected to local kafka server on port 9092 ...');
-}
-).catch(e => { console.log('Error : ', e) });
+}).catch(e => {
+  console.log('Error : ', e)
+});
 
 const {
   createPool,
 } = require('./common/postgresWrapper');
-database=config.get('POSTGRES.database');
+database = config.get('POSTGRES.database');
 const pool = createPool(database);
 pool.on('remove', client => {
-console.log("setting property to on query completion");
+  console.log("setting property to on query completion");
 })
 console.log('---------------------------------');
 
 const dataHandler = function (messageSet, topic, partition) {
-    return Promise.each(messageSet, async function (m) {
-      const payload = JSON.parse(m.message.value)
+  return Promise.each(messageSet, async function (m) {
+    const payload = JSON.parse(m.message.value)
 
-      // insert consumer_log
-      consumerLog({
+    // insert consumer_log
+    consumerLog({
         SEQ_ID: payload.SEQ_ID,
         TOPICNAME: topic,
         SCHEMA_NAME: payload.SCHEMANAME,
@@ -41,34 +56,32 @@ const dataHandler = function (messageSet, topic, partition) {
         },
         DESTINATION: config.DESTINATION
       }).then(log => console.log('Add Consumer Log'))
-        .catch(err => console.log(err))
+      .catch(err => console.log(err))
 
-      //update postgres table
-      let postgreErr
-      if(payload.uniquedatatype === 'true') {
-        //retrive teh data from info and insert in postgres
-        console.log("welcome")
-        //await migrateinsertdata(payload, pool)
-        console.log(pool);
-        if(payload.OPERATION === 'INSERT') {
-            await migrateifxinsertdata(payload, pool)
-            .catch(err => {
-              postgreErr = err
-              console.log(err)
-            })            
-         }
-        if(payload.OPERATION === 'UPDATE') {
-            await migrateifxupdatedata(payload, pool)
-            .catch(err => {
-              postgreErr = err
-              console.log(err)
-            })            
-         }
-        console.log("Different approach")
+    //update postgres table
+    let postgreErr
+    if (payload.uniquedatatype === 'true') {
+      //retrive teh data from info and insert in postgres
+      console.log("welcome")
+      //await migrateinsertdata(payload, pool)
+      console.log(pool);
+      if (payload.OPERATION === 'INSERT') {
+        await migrateifxinsertdata(payload, pool)
+          .catch(err => {
+            postgreErr = err
+            console.log(err)
+          })
       }
-      else
-      {
-      if(payload.OPERATION === 'INSERT') {
+      if (payload.OPERATION === 'UPDATE') {
+        await migrateifxupdatedata(payload, pool)
+          .catch(err => {
+            postgreErr = err
+            console.log(err)
+          })
+      }
+      console.log("Different approach")
+    } else {
+      if (payload.OPERATION === 'INSERT') {
         let entity = payload.DATA
         await migratepgInsert(pool, entity, payload.SCHEMANAME, payload.TABLENAME)
           .catch(err => {
@@ -76,14 +89,14 @@ const dataHandler = function (messageSet, topic, partition) {
             console.log(err)
           })
 
-      } else if(payload.OPERATION === 'UPDATE') {
+      } else if (payload.OPERATION === 'UPDATE') {
         await migratepgUpdate(pool, payload.DATA, payload.SCHEMANAME, payload.TABLENAME)
           .catch(err => {
             postgreErr = err
             console.log(err)
           })
 
-      } else if(payload.OPERATION === 'DELETE') {
+      } else if (payload.OPERATION === 'DELETE') {
         let entity = payload.DATA
         await migratepgDelete(pool, entity, payload.SCHEMANAME, payload.TABLENAME)
           .catch(err => {
@@ -93,27 +106,30 @@ const dataHandler = function (messageSet, topic, partition) {
 
       }
     }
-      //audit success log
-      if(!postgreErr){
+    //audit success log
+    if (!postgreErr) {
       await cAuditLog({
-        SEQ_ID: payload.SEQ_ID,
-        CONSUMER_DEPLOY_STATUS: 'success',
-        CONSUMER_UPDATE_TIME: Date.now()
-      }).then(log => console.log('postgres '+ payload.OPERATION + ' success'))
+          SEQ_ID: payload.SEQ_ID,
+          CONSUMER_DEPLOY_STATUS: 'success',
+          CONSUMER_UPDATE_TIME: Date.now()
+        }).then(log => console.log('postgres ' + payload.OPERATION + ' success'))
         .catch(err => console.log(err))
 
-      return consumer.commitOffset({ topic: topic, partition: partition, offset: m.offset, metadata: 'optional' })
-      }
-
-//================      
+      return consumer.commitOffset({
+        topic: topic,
+        partition: partition,
+        offset: m.offset,
+        metadata: 'optional'
+      })
+    } else {
 
       //audit failure log
       await cAuditLog({
-        SEQ_ID: payload.SEQ_ID,
-        CONSUMER_DEPLOY_STATUS: 'failure',
-        CONSUMER_FAILURE_LOG: postgreErr,
-        CONSUMER_UPDATE_TIME: Date.now()
-      }).then((log) => console.log('postgres '+ payload.OPERATION + ' failure'))
+          SEQ_ID: payload.SEQ_ID,
+          CONSUMER_DEPLOY_STATUS: 'failure',
+          CONSUMER_FAILURE_LOG: postgreErr,
+          CONSUMER_UPDATE_TIME: Date.now()
+        }).then((log) => console.log('postgres ' + payload.OPERATION + ' failure'))
         .catch(err => console.log(err))
 
       let msgValue = {
@@ -122,135 +138,86 @@ const dataHandler = function (messageSet, topic, partition) {
         payloadposted: JSON.stringify(payload)
       }
 
-//===================================
-
       if (!payload.retryCount) {
         payload.retryCount = 0
         logger.debug('setting retry counter to 0 and max try count is : ', config.KAFKA_REPOST_COUNT);
       }
       if (payload.retryCount >= config.KAFKA_REPOST_COUNT) {
         logger.debug('Recached at max retry counter, sending it to error queue: ', config.topic_error.NAME);
-        await producer.send({
-          topic: config.topic_error.NAME,
-          partition: config.topic_error.PARTITION,
-          message: {
-              value : JSON.stringify(msgValue),
-          }
-          },{
-            retries: {
-              attempts: config.RETRY_COUNTER,
-              delay: {
-                min: 100,
-                max: 300
-              }
-            }
-          }).then(function (result) {
-            console.log(result)
-        })
-      }
-      else
-      {
-         payload['retryCount'] = payload.retryCount + 1;
+        kafka_error = await pushToKafka(producer, config.topic_error.NAME, msgValue)
 
-         let seqID = 0
-         //add producer_log
-         await producerLog({
-           TOPICNAME: config.topic.NAME,
-           SOURCE: config.SOURCE,
-           SCHEMA_NAME: payload.SCHEMANAME,
-           TABLE_NAME: payload.TABLENAME,
-           PRODUCER_PAYLOAD: payload.DATA,
-           OPERATION: payload.OPERATION
-         }).then((log) => seqID = log.SEQ_ID)
-       
-         if(!seqID){
-           console.log('ProducerLog Failure')
-           return
-         }
-         console.log('ProducerLog Success')
-         payload['SEQ_ID'] = seqID;
-         //SEQ_ID: seqID
-         await producer.send({
-          topic: config.topic.NAME,
-          partition: config.topic.PARTITION,
-          message: {
-            value : JSON.stringify(payload)
-          }
-        },{
-          retries: {
-            attempts: config.RETRY_COUNTER,
-            delay: {
-              min: 100,
-              max: 300
-            }
-          }
-        }).then(function (result) {
-            if(result[0].error)
-              kafka_error = result[0].error
+      } else {
+        payload['retryCount'] = payload.retryCount + 1;
+        let seqID = 0
+        //add producer_log
+        await producerLog({
+          TOPICNAME: config.topic.NAME,
+          SOURCE: config.SOURCE,
+          SCHEMA_NAME: payload.SCHEMANAME,
+          TABLE_NAME: payload.TABLENAME,
+          PRODUCER_PAYLOAD: payload.DATA,
+          OPERATION: payload.OPERATION
+        }).then((log) => seqID = log.SEQ_ID)
 
-            console.log(kafka_error)  
-        })
-
-  //add auditlog
-  if(!kafka_error){
-    await pAuditLog({
-      SEQ_ID: seqID,
-      PRODUCER_PUBLISH_STATUS: 'success',
-      PRODUCER_PUBLISH_TIME: Date.now()
-    }).then((log) => console.log('Send Success'))
-    //res.send('done')
-    return consumer.commitOffset({ topic: topic, partition: partition, offset: m.offset, metadata: 'optional' })
-    }
-  
-    //add auditlog
-    await pAuditLog({
-      SEQ_ID: seqID,
-      PRODUCER_PUBLISH_STATUS: 'failure',
-      PRODUCER_FAILURE_LOG: kafka_error,
-      PRODUCER_PUBLISH_TIME: Date.now()
-    }).then((log) => console.log('Send Failure'))
-  
-    msgValue = {
-      ...kafka_error,
-      SEQ_ID: seqID,
-      recipients: config.topic_error.EMAIL,
-      msgoriginator: "consumer-producer"
-    }
-  
-    //send error message to kafka
-    await producer.send({
-      topic: config.topic_error.NAME,
-      partition: config.topic_error.PARTITION,
-      message: {
-        value : JSON.stringify(msgValue),
-      }
-      },{
-        retries: {
-          attempts: config.RETRY_COUNTER,
-          delay: {
-            min: 100,
-            max: 300
-          }
+        if (!seqID) {
+          console.log('ProducerLog Failure')
+          return
         }
-      }).then(function (result) {
-        console.log(result)
-      })        
-      
- //await auditTrail([message.payload.payloadseqid,cs_processId,message.payload.table,message.payload.Uniquecolumn,
-   //       message.payload.operation,"Error",message.payload['retryCount'],err.message,"",message.payload.data, message.timestamp,message.topic],'consumer')
-     // await pushToKafka(message)      
+        console.log('ProducerLog Success')
+        payload['SEQ_ID'] = seqID;
+        //SEQ_ID: seqID
+        kafka_error = await pushToKafka(producer, config.topic.NAME, payload)
+        //add auditlog
+        if (!kafka_error) {
+          await pAuditLog({
+            SEQ_ID: seqID,
+            PRODUCER_PUBLISH_STATUS: 'success',
+            PRODUCER_PUBLISH_TIME: Date.now()
+          }).then((log) => console.log('Send Success'))
+          //res.send('done')
+          return consumer.commitOffset({
+            topic: topic,
+            partition: partition,
+            offset: m.offset,
+            metadata: 'optional'
+          })
+        } else {
+          //add auditlog
+          await pAuditLog({
+            SEQ_ID: seqID,
+            PRODUCER_PUBLISH_STATUS: 'failure',
+            PRODUCER_FAILURE_LOG: kafka_error,
+            PRODUCER_PUBLISH_TIME: Date.now()
+          }).then((log) => console.log('Send Failure'))
+
+          msgValue = {
+            ...kafka_error,
+            SEQ_ID: seqID,
+            recipients: config.topic_error.EMAIL,
+            msgoriginator: "consumer-producer"
+          }
+          //send error message to kafka
+          kafka_error = await pushToKafka(producer, config.topic_error.NAME, msgValue)
+        }
       }
       //send postgres_error message
 
-//===============================================
+      //===============================================
       // commit offset
-      return consumer.commitOffset({ topic: topic, partition: partition, offset: m.offset, metadata: 'optional' })
-    }).catch(err => console.log(err))
+      return consumer.commitOffset({
+        topic: topic,
+        partition: partition,
+        offset: m.offset,
+        metadata: 'optional'
+      })
+    }
+  }).catch(err => console.log(err))
+
 };
 
 const strategies = [{
-    subscriptions: [config.topic.NAME],
-    handler: dataHandler
+  subscriptions: [config.topic.NAME],
+  handler: dataHandler
 }];
 
 consumer.init(strategies);
